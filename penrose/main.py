a24bf824964aa6272ccdc4a9837ee7714f86ac1e
ThomasCWright <tcw2512@gmail.com>
2020-12-03 21:08:45 +0100

    minimal app useful for assessing appium

--- penrose/main.py
+++ penrose/main.py
@@ -1,18 +1,37 @@
 import kivy
 
-kivy.require('2.0.0') # replace with your current kivy version !
+kivy.require('1.11.1') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 
-class MyApp(App):
+
+class DartScreen(BoxLayout):
 
     def build(self):
-        return Label(text='Hello world')
+        pass
+
+
+    def change_source(self):
+        if self.ids.tile_image.source == 'penrose_dart.png':
+            self.ids.tile_image.source = 'penrose_kite.png'
+        else:
+            self.ids.tile_image.source = 'penrose_dart.png'
+        print(self.ids.tile_image.source)
+        self.ids.tile_image.reload()
+        pass
+
+
+class PenroseApp(App):
+
+    def build(self):
+        # return Label(text='Hello world')
+        return DartScreen()
 
 def init():
     if __name__ == '__main__':
-        MyApp().run()
+        PenroseApp().run()
 
 init()
